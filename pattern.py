ab9da4aa0130bde7ffbc6defda44ea9abe3c07a5
anasmuhd01 <anasmuhd010@gmail.com>
2026-01-29 11:46:54 +0530

    star reverse

--- pattern.py
+++ pattern.py
@@ -1,5 +1,28 @@
 #print  given star pattern with user input
-for i in range(5):
-    for j in range(i+1):
+# for i in range(5):
+#     for j in range(i+1):
+#         print("* ",end="")
+#     print()
+
+
+
+# for i in range(5):
+#     star=""
+#     for j in range(i+1):
+#         star+="* "
+#     print(star)
+
+
+# for i in range(5,0,-1):
+#     star=""
+#     for j in range(i):
+#         star+="* "
+#     print(star)
+
+
+inp=int(input("enter limit: "))
+
+for i in range(inp,0,-1):
+    for j in range(i):
         print("* ",end="")
     print()
