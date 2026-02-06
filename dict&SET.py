13273745172989ac4293db594e5518e8f219e99f
anasmuhd01 <anasmuhd010@gmail.com>
2026-02-06 12:20:56 +0530

    dict problems

--- dict&SET.py
+++ dict&SET.py
@@ -54,10 +54,46 @@ a={"name":"ajith","place":"calicut","age":20}
 # a.update({"name":"ajith kk"})
 # print(a)
 
-a={1,2,3,3,"apple"}
-print(a)
-a.add("kiwi")
-print(a)
-a.remove("kiwi")
-a.remove(3)
-print(a)
+# a={1,2,3,3,"apple"}
+# print(a)
+# a.add("kiwi")
+# print(a)
+# a.remove("kiwi")
+# a.remove(3)
+# print(a)
+
+
+# a ={}
+
+# limit = int(input("enter limit: "))
+
+# for i in range(limit):
+
+#     key=input("enter key: ")
+#     valu=input("enter value:  ")
+
+#     a.setdefault(key,valu)
+
+#     a.update({key:valu})
+
+# print(a)
+
+# s = {*()} #--> set 
+# print(type(s))
+
+st ="apple apple apple banana grape grape"
+# st = input("enter your string: ")
+emp_di={}
+
+word_split = st.split(" ")
+
+
+for i in word_split:
+    if i in emp_di:
+        emp_di[i]+=1
+    else:
+        emp_di[i]=1
+
+print(emp_di)
+
+    
