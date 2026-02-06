ae4d52b74a4c465aa3ef74ffe7fb826d2d84fd08
anasmuhd01 <anasmuhd010@gmail.com>
2026-02-06 11:31:27 +0530

    dictionary methods

--- dict.py
+++ dict.py
@@ -22,9 +22,34 @@ a={"name":"ajith","place":"calicut","age":20}
 # a.clear()
 # print(a)
 
-res=dict.fromkeys(["name","age","place"])
-print(res)
+# res=dict.fromkeys(["name","age","place"])
+# print(res)
 
-res2=a.get("name")
-print(res2)
+# res2=a.get("name")
+# print(res2)
 
+# res=a.items()
+# print(res)
+
+# for i in res:
+#     print(i)
+    
+# print(a.keys())
+# print(a.values())
+
+# res=a.pop("age")
+# print(res)
+# print(a)
+
+# a.popitem()
+# print(a)
+
+x= a.setdefault("name",'ajithk')
+print(x)
+print(a)
+
+a.setdefault("last_test",123)
+print(a)
+
+a.update({"name":"ajith kk"})
+print(a)
