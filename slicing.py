5ce6bbf734f4e569dfb888ed31bc225cce4d2f2c
anasmuhd01 <anasmuhd010@gmail.com>
2026-02-09 11:31:03 +0530

    slicing ::

--- slicing.py
+++ slicing.py
@@ -4,17 +4,30 @@
 
 #---------------STRING------------------------
 #--0123456789
-a="one two three"
-res=a[4:7]
-print(res)
+# a="one two three"
+# res=a[4:7]
+# print(res)
 
-#---------------LIST------------------------
-b=[1,2,3,4,"apple","kiwi"]
-res=b[:4]
-print(res)
+# #---------------LIST------------------------
+# b=[1,2,3,4,"apple","kiwi"]
+# res=b[:4]
+# print(res)
 
 
 #---------------TUPLE-----------------------
-c=(1,2,3,4,"apple","kiwi")
-res=b[1:5]
-print(res)
+# c=(1,2,3,4,"apple","kiwi")
+# res=c[1:5]
+# res1=c[::-1]
+# res2=c[-3::]
+
+# print(res)
+# print(res1)
+# print(res2)
+
+
+# a="this is a string"
+
+# res=a[::-1]
+# res=a[0::2]
+
+# print(res)
