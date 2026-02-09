84c14aea4f07ddf66c11fcb3597ecd141319d79f
anasmuhd01 <anasmuhd010@gmail.com>
2026-02-09 12:27:36 +0530

    list compre problems:

--- listcomp.py
+++ listcomp.py
@@ -1,8 +1,43 @@
 a=[12,23,14,56,78,6,108,91,38]
-em=[]
+# em=[]
 
-for i in a:
-    if i % 2 ==0:
-        em.append(i)
-        
-print(em)
+# for i in a:
+#     if i % 2 ==0:
+#         em.append(i)
+
+# print(em)
+
+# # even=[]
+# em=[i for i in a if i%2 != 0]
+# print(em)
+
+
+
+# em1=[i**2 for i in a ]
+# print(em1)
+
+a=["apple","banana","kiwi","lichi","strawberry","cherry","avocado","tomato","orange"]
+
+# for i in a:
+#     print(i)
+#     x=
+
+# x=[i for i in a if len(i)>6]
+# print(x)
+
+
+x =[i[::-1] for i in a ]
+print(x)
+# x=[]
+# for i in a:
+    
+#     y=(i[::-1])
+#     x.append(y)
+
+# print(x)
+
+
+
+
+
+
