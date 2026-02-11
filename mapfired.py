c91921dc6d296816d5131b9fa216876b9bc89813
anasmuhd01 <anasmuhd010@gmail.com>
2026-02-11 22:13:56 +0530

    map filter reduce

---
+++ mapfired.py
@@ -0,0 +1,33 @@
+# from functools import reduce
+# a=[12,4,15,67,82,93,109,234]
+
+
+# em=[]
+# for i in a:
+#     cube=i**3
+#     em.append(cube)
+
+# # print(em)
+
+# def cube(a):
+#     return a**3
+
+# a=[12,4,15,67,82,93,109,234]
+
+# res=map(cube,a)
+# print(list(res))
+# x=lambda a:a**3
+
+
+# res = map(x,a)
+
+# res = map(lambda a:a**3,a)
+# print(list(res))
+
+# res = filter(lambda a:a%2==0,a)
+
+# print(list(res))
+
+a=["apple","banana","strawbery","kiwi","orange"]
+
+print(list(filter(lambda s:len(s)>=5,a)))
