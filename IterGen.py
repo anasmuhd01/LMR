27b99b3811ee3dab676b87d2897ed072d2473b67
anasmuhd01 <anasmuhd010@gmail.com>
2026-02-16 11:56:10 +0530

    generator

--- IterGen.py
+++ IterGen.py
@@ -6,3 +6,22 @@
 # print(next(it))
 # print(next(it))
 
+# def fun():
+#     print("Hello World")
+#     return 1
+#     print("hello") # function ends with return
+
+# res = fun()
+# print(res)
+
+def sample():
+    print("hello world")
+    yield 1
+    yield 2
+    yield 3
+
+
+res=sample() # <generator object fun at 0x0000013C75295E40>
+print(next(res))
+print(next(res))
+print(next(res))
