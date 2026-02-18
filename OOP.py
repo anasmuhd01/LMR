bb3485064c36e779270a58d0a29c536f4d4bdcae
anasmuhd01 <anasmuhd010@gmail.com>
2026-02-18 11:39:52 +0530

    dunder  methods

--- OOP.py
+++ OOP.py
@@ -40,17 +40,43 @@
 # # print(dell.processor)
 # dell.booting()
 
-class Arith:
-    def __init__(self,val1,val2):
-        self.val1=val1
-        self.val2=val2
+# class Arith:
+#     def __init__(self,val1,val2):
+#         self.val1=val1
+#         self.val2=val2
 
-    def add(self):
-        return self.val1+self.val2
+#     def add(self):
+#         return self.val1+self.val2
     
-    def po(self,n):
-        print(self.val1**n)
+#     def po(self,n):
+#         print(self.val1**n)
     
 
-add=Arith(10,20)
-print(add.add())
+# add=Arith(10,20)
+# print(add.add())
+
+
+#WAP where a class student is created for storing student name
+#and marks from two subject in an object  define method for calculating 
+#toatl marks and average marks of each student
+
+class Student:
+    def __init__(self,name,mark1,mark2):
+        self.name= name
+        self.mark1=mark1
+        self.mark2=mark2
+
+    def toatl_mark(self):
+        # global total
+        total=self.mark1+self.mark2
+        print(total)
+
+    def average_mark(self):
+        avg=(self.mark1+self.mark2)/2
+        print(avg)
+
+stu1=Student("Anas",10,20)
+stu1.toatl_mark()
+stu1.average_mark()
+
+        
