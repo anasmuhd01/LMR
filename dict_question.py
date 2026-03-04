73780f70177308dff17f20ff1aadfaed8edecbbe
anasmuhd01 <anasmuhd010@gmail.com>
2026-03-04 18:20:59 +0530

    task4

---
+++ dict_question.py
@@ -0,0 +1,56 @@
+phonebook={}
+x=5
+
+
+while True:
+    operation=int(input("\npress number for operatio \n1.CREATE: \n2.RETRIEVE: \n3.UPDATE: \n4.DELETE \n5.EXIT\n"))
+    if operation==1:
+        name=input("enter name: ")
+        phone=input("enter phone: ")
+        phonebook.update({name:{"name":name,"phone":phone}})
+        
+
+    elif operation==2:
+        if len(phonebook)==0:
+            print("No elements to show")
+        else:
+            for i in phonebook:
+                print(f"\n{phonebook[i]["name"]}:{phonebook[i]["phone"]}")
+        
+
+    elif operation==3:
+        if len(phonebook)==0:
+            print("No elements to update add item:3")
+        else:
+            print("\n names in directory:")
+            for i in phonebook:
+                print(f"names:{phonebook[i]["name"]}")
+
+                name=input("\nenter name to update phone:")
+            
+                if name==phonebook[i]["name"]:
+                    num=input("enter number: ")
+                    phonebook[i]["phone"]=num
+                else:
+                    print("invalid name")
+
+
+    elif operation==4:
+        print("available names: ")
+        for i in phonebook:
+            print(f"\n{phonebook[i]["name"]}")
+        name=input("enter name")
+        if name in phonebook:
+            phonebook.pop(name)
+    elif operation==5:
+        break
+         
+        
+
+
+
+    else:
+        print("invalid operation")
+
+
+
--- task4.py
+++ task4.py
@@ -1,56 +1,91 @@
-phonebook={}
-x=5
+#1
+# num=int(input("Enter number: "))
+# if num%2==0:
+#     print(f"{num} is even number!")
+# else:
+#     print(f"{num} is odd")
 
+#2
+# num=int(input("enter year: "))
+# if num%400==0:
+#     print("Leap Year")
+# elif num%100==0:
+#     print("Not leap year")
+# elif num%4==0:
+#     print("leap year:")
+# else:
+#     print("Not leap year")
 
-while True:
-    operation=int(input("\npress number for operatio \n1.CREATE: \n2.RETRIEVE: \n3.UPDATE: \n4.DELETE \n5.EXIT\n"))
-    if operation==1:
-        name=input("enter name: ")
-        phone=input("enter phone: ")
-        phonebook.update({name:{"name":name,"phone":phone}})
+#3
+# a=[]
+# for i in range(3):
+#     value=int(input("enter number: "))
+#     a.append(value)
+
+# if a[0]>a[1] and a[0]>a[2]:
+#     print(f"{a[0]} is greater")
+# elif a[1]>a[2]:
+#     print(f"{a[1]} is greater")
+# else:
+#     print(f"{a[2]} is greater")
+
+#4
+
+#7
+# num1=int(input("Enter num1: "))
+# num2=int(input("Enter num2: "))
+# oper=input("Enter Operation: \n + * / % - \n")
+
+# def math_operations(num1,num2,oper):
+#     if oper=="+":
+#         return num1+num2
+#     elif oper=="-":
+#         return num1-num2
+#     elif oper=='/':
+#         return num1/num2
+#     elif oper=='%':
+#         return num1%num2
+#     elif oper=='*':
+#         return num1*num2
+#     else:
+#         return f"{oper} is not a valid operation:"
+
+# res=math_operations(num1,num2,oper)
+# print(res)
+
+#8
+# mark=int(input("enter mark: "))
+# if mark >80:
+#     print("A")
+# elif mark >=60 and mark<80:
+#     print("B")
+# elif mark >=50 and mark<60:
+#     print("C")
+# elif mark >=45 and mark<50:
+#     print("D")
+# elif mark >=25 and mark<45:
+#     print("E")
+# else:
+#     print("F")
+
+#9
+
+# city_moun={
+#     "Delhi":"Red fort",
+#     "Agra":"Taj mahal",
+#     "Jaipur":"Jal mahal"
+#            }
+# city=input("Enter city: ")
+
+# if city in city_moun:
+#     print(city_moun[city])
+# else:
+#     print("Invalid city")
+
+#10
+
         
-
-    elif operation==2:
-        if len(phonebook)==0:
-            print("No elements to show")
-        else:
-            for i in phonebook:
-                print(f"\n{phonebook[i]["name"]}:{phonebook[i]["phone"]}")
-        
-
-    elif operation==3:
-        if len(phonebook)==0:
-            print("No elements to update add item:3")
-        else:
-            print("\n names in directory:")
-            for i in phonebook:
-                print(f"names:{phonebook[i]["name"]}")
-
-                name=input("\nenter name to update phone:")
-            
-                if name==phonebook[i]["name"]:
-                    num=input("enter number: ")
-                    phonebook[i]["phone"]=num
-                else:
-                    print("invalid name")
-
-
-    elif operation==4:
-        print("available names: ")
-        for i in phonebook:
-            print(f"\n{phonebook[i]["name"]}")
-        name=input("enter name")
-        if name in phonebook:
-            phonebook.pop(name)
-    elif operation==5:
-        break
-         
-        
-
-
-
-    else:
-        print("invalid operation")
+    
 
 
 
