9eba4daf3e177ed36fe4801bdba0814e6a0635b7
anasmuhd01 <anasmuhd010@gmail.com>
2026-03-25 21:15:47 +0530

    data added

--- phonebookDB.py
+++ phonebookDB.py
@@ -2,5 +2,12 @@ import pymysql
 connection = pymysql.connect(host="localhost", user="root",password="root",port=3306,database="phonebook")
 cursor = connection.cursor()
 
-# cursor.execute("CREATE TABLE customer (name VARCHAR(100), contact BIGINT)")
+# cursor.execute("CREATE TABLE customer (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,name VARCHAR(100), contact BIGINT)")
+# # cursor.execute("DROP TABLE customer")
+               
 # print("table created")
+
+# qry=f"insert into customer values(null,'test1','123456')"
+# cursor.execute(qry)
+# connection.commit()
+# print("Data added")
