d6d6e1456c994552b73cd25fbd623161fe1ffdd6
unknown <anasmuhd010@gmail.com>
2026-04-18 13:39:01 +0530

    looping FOR

--- exercise'/one.py
+++ exercise'/one.py
@@ -1,9 +1,9 @@
-for i in range(5,0,-1):
-    for j in range(i):
-        print("  ",end=" ")
-    for k in range(5-i+1):
-        print("  * ",end="  ")
-    print()
+# for i in range(5,0,-1):
+#     for j in range(i):
+#         print("  ",end=" ")
+#     for k in range(5-i+1):
+#         print("  * ",end="  ")
+#     print()
 
 # for i in range(6):
 #     for j in range(i+1):
@@ -14,6 +14,12 @@ for i in range(5,0,-1):
 #     for j in range(i-1):
 #         print("*", end=" ")
 #     print()
+
+for i in range(1,6):
+    for j in range(6-i):
+        print("",end=" ")
+    
+    print()
 
 
     
