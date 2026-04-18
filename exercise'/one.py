bd862ebe8cfa7bff06be6bcf175f7415a3cb795b
unknown <anasmuhd010@gmail.com>
2026-04-18 13:46:40 +0530

    pattern completed

--- exercise'/one.py
+++ exercise'/one.py
@@ -17,10 +17,10 @@
 
 for i in range(1,6):
     for j in range(6-i):
-        print("",end=" ")
-    
+        print(" ",end="")
+    for k in range(i):
+        print("*",end=" ")
     print()
-
 
     
     
