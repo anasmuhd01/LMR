fcbcdbd37dedb914ebeb71eeca1c767c8e572dcd
unknown <anasmuhd010@gmail.com>
2026-04-18 15:00:49 +0530

    class eg:

--- exercise'/one.py
+++ exercise'/one.py
@@ -15,14 +15,31 @@
 #         print("*", end=" ")
 #     print()
 
-for i in range(1,6):
-    for j in range(6-i):
-        print(" ",end="")
-    for k in range(i):
-        print("*",end=" ")
-    print()
+# for i in range(1,6):
+#     for j in range(6-i):
+#         print(" ",end="")
+#     for k in range(i):
+#         print("*",end=" ")
+#     print()
+
 
     
     
   
-    
+# class MyClass:
+#     x=123
+
+# class tempClass:
+#     pass
+ 
+# p1=MyClass()
+
+# print(p1.x)
+class Person:
+  def __init__(self,name,age):
+    self.name=name
+    self.age=age
+  def greet(self):
+    print(f"Hello, my name is {self.name}")
+p1= Person( "John",36)
+p1.greet()
