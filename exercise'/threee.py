3ecf267f87b98a75d58a77132df1250418ddeff4
unknown <anasmuhd010@gmail.com>
2026-04-22 20:59:17 +0530

    pattern 6,7

--- exercise'/threee.py
+++ exercise'/threee.py
@@ -5,7 +5,34 @@ limit =5
 #     print()
 
 
-for i in range(limit+1):
-    for j in range(i):
-        print(i,end=" ")
-    print()
+# for i in range(limit+1):
+#     for j in range(i):
+#         print(i,end=" ")
+#     print()
+
+# for i in range(limit):
+#     for j in range(5,i,-1):
+#         print("*",end=" ")
+#     print()
+
+# for k in range(2,5+1):
+#     for l in range(k):
+#         print("*",end=" ")
+#     print()
+
+# for i in range(limit):
+#     for j in range(5,i,-1):
+#         print( "",end=" ")
+#     for k in range(i):
+#         print("*",end=" ")
+#     print()
+
+# for i in range(1,limit+1):
+#     for j in range(1,6):
+#         print(j,end=" ")
+#     print()
+
+# for i in range(1,limit+1):
+#     for j in range(1,i+1):
+#         print(j,end=" ")
+#     print()
