183064765cf65946d2a60ae216a0acae8cba03bc
unknown <anasmuhd010@gmail.com>
2026-04-22 19:56:37 +0530

    star pattern 1

---
+++ exercise'/threee.py
@@ -0,0 +1,8 @@
+limit =5
+# for i in range ( 1,limit+1):
+#     for j in range(6,i,-1):
+#         print("*",end=" ")
+#     print()
+
+
+for i in range()
---
+++ exercise'/two.py
@@ -0,0 +1,8 @@
+#print hellow world 
+# print("Hello World")
+
+# Python program to add two numbers
+a=1
+b=2
+sum= a+b
+print(sum)
