e6762d29a57cd67b428b9ea84ae694d63dd56fb4
anasmuhd01 <anasmuhd010@gmail.com>
2026-06-11 19:42:08 +0530

    palindrome

--- DailyTask/day1.py
+++ DailyTask/day1.py
@@ -1,5 +1,12 @@
 # Reverse a string:
-string1 = 'hellow'
-print(string1[::-1])
+# string1 = 'hellow'
+# print(string1[::-1])
 
 # Check palindrome
+print('enter string: ')
+string1 = input()
+pal = string1[::-1]
+if string1 == pal:
+    print("palindrome")
+else:
+    print('not palindrome')
