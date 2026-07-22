c2bdc1853ece9f6f14f0019f21630c876453e717
anasmuhd01 <anasmuhd010@gmail.com>
2026-07-22 22:44:30 +0530

    all

--- tasks.py
+++ tasks.py
@@ -40,9 +40,46 @@
 #         check.append(i)
 
 # print(check)
-nums = [1,2,3,4,5]
+# nums = [1,2,3,4,5]
 
-sum =0
-for i in nums :
-    sum += i
-print(sum)
+# sum =0
+# for i in nums :
+#     sum += i
+# print(sum)
+
+# numbers = [23, 5, 78, 1, 45]
+# first = numbers[0]
+# for i in numbers:
+#     if first > i:
+#         first =i
+# print(first)
+
+# li = [1,2,3,4,5]
+
+# sum =0
+# for i in li:
+#     sum+= i
+# print(sum)
+
+# li =[10,20,30,40]
+# sum = 0
+# avg =0
+# for i in li:
+#     sum+=i
+
+# avg = sum/len(li)
+# print(avg)
+# import re
+# word = "AnAsMuhammed"
+# cap = "[A-Z]"
+# lower = "[a-z]"
+# res =re.findall(cap,word)
+# res2 = re.findall(lower,word)
+# print(len(res))
+# print(f'total lowrcase letters {len(res2)}')
+
+
+# li = "abc12345"
+# import re 
+# pat = "[0-9]"
+# print(len(re.findall(pat,li)))
